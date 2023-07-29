60d775ccb5ed3f39d1b6e69d5b5281f882366c8b
Dushyant2910 <140684872+Dushyant2910@users.noreply.github.com>
2023-07-29 18:25:46 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
 [default]
-aws_access_key_id - AKIAXQP7BUKTBQMBDIUW
-aws_secret_access_key - 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
+aws_access_key_id - AKIAXQP7BUKTG5MQGEK2
+aws_secret_access_key - 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
